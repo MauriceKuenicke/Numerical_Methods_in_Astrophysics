bf99ccaa22f318216ca5063e374c9c041562c877
LimaKina <georg.noffz@googlemail.com>
2020-11-25 16:14:39 +0100

    added scripts provided by course webpage

---
+++ session_03/bug03.f90
@@ -0,0 +1,42 @@
+! a short program that computes mean and standard deviation
+PROGRAM Statistics
+  IMPLICIT NONE
+
+  INTEGER                            :: i, n
+  REAL(8), DIMENSION(:), ALLOCATABLE :: values, input
+  REAL(8)                            :: mean, stddev, stddev2, offset
+
+  ! read in the number of numbers
+  PRINT*, 'how many numbers?'
+  READ*, n
+
+  ! allocate memory
+  ALLOCATE(input(n), values(n))
+
+  ! read in values (loop not needed)
+  READ*, input
+
+  DO i=0,6
+
+    offset = 10**i
+
+    values = input + offset
+
+    ! compute mean using intrinsic sum
+    mean   = SUM(values)/REAL(n)
+
+    ! compute sample standard deviation
+    stddev = SQRT( SUM((values - mean)**2) / (REAL(N-1)) )
+
+    ! mathematically identical way to compute the standard deviation
+    stddev2= SQRT( REAL(N)/REAL(N-1)*(SUM(values**2)/REAL(N) - mean**2) )
+
+    PRINT '(3(A,ES20.8))', "mean = ", mean, " stddev = ", stddev, &
+&                                           " stddev2 = ", stddev2
+
+  END DO
+
+  ! free memory
+  DEALLOCATE(values)
+
+END PROGRAM Statistics
---
+++ session_03/plot_complex.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+#  this script plots a map for 3d data from a root finding program 
+#  the data should be in the format
+#
+#      x       y       irr         k
+#
+#  where irr=1 whenever a real root was found (0 for complex root)
+#  and k is number of iterations erquired when starting at point
+#  z = complex(x, y)
+# 
+#  for high-resolution data files (N>1000000) plot with dots
+#  (commented out below)
+#
+
+# data file name can be given as command-line option, otherwise
+# default is used
+datafile=${1:-complexnewton.dat}
+
+# run gnuplot to create plot
+gnuplot <<EOF
+set size ratio 1
+set border 31 lw 2
+
+set xrange [-1.0 to 1.0]
+set yrange [-1.0 to 1.0]
+set zrange [0.5 to 1.5]
+
+set xlabel 'x'
+set ylabel 'y'
+
+unset key
+set view map
+
+
+set term png font "/usr/share/fonts/truetype/freefont/FreeSans.ttf" 16 size 600, 600
+set out 'real_root.png'
+
+splot '$datafile' using 1:2:3 with points pointtype 7 pointsize 0.5
+#splot '$datafile' using 1:2:3 with dots
+
+set autoscale z
+set cbtics 10
+set out 'convergence.png'
+
+splot '$datafile' using 1:2:4 with points palette pt 7 ps 1
+#splot '$datafile' using 1:2:4 with dots palette 
+
+
+ 
+EOF
