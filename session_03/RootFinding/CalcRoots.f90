d82430e90ea388a229a798266f36d2cd36a0f93b
Maurice Künicke <m.kuenicke1995@gmail.com>
2020-11-30 22:44:48 +0100

    Make algorithm stop after 100000 failed iterations

--- session_03/RootFinding/CalcRoots.f90
+++ session_03/RootFinding/CalcRoots.f90
@@ -4,9 +4,11 @@ program CalcRoots
     implicit none
 
     type(myroot_type) :: r
+    logical :: export_accuracy
 
-    r = find_root_newton(func3, dfunc3, 2.1d0)   ! find_root_newton(func, func_derivative, init_guess) or find_root_bisection(func, a, b)
-    !r = find_root_bisection(func2, 0.d0, 2.9d0)   
+    export_accuracy = .TRUE.   ! .FALSE. or .TRUE.
+    r = find_root_newton(func2, dfunc2, -5.d0, export_accuracy)   ! find_root_newton(func, func_derivative, init_guess) or find_root_bisection(func, a, b)
+    !r = find_root_bisection(func2, -10.d0, 10.d0, export_accuracy)   
     
     write(*, '(2X, "Root", 23X, "Epsilon", 19X, "Value", 28X,"Iterations")')
     print*, r
--- session_03/RootFinding/RootFinding.f90
+++ session_03/RootFinding/RootFinding.f90
@@ -9,13 +9,18 @@ module RootFinding
     
 contains
 
-function find_root_bisection(func, a, b) result(res)
+function find_root_bisection(func, a, b, export_accuracy) result(res)
     implicit none
     real(kind=8) :: bracket_start, bracket_end, func, a, b
     type(myroot_type) :: res
+    logical :: export_accuracy
 
     real(kind=8) :: midpoint, func_value_midpoint, diff, func_value_lower, func_value_higher
     integer :: iter
+
+    if (export_accuracy) then
+        open(20, file="data/accuracy_bisection.dat")
+    end if
     
     bracket_start = a
     bracket_end = b
@@ -36,6 +41,15 @@ function find_root_bisection(func, a, b) result(res)
 
         diff = abs(bracket_start-bracket_end)
         iter = iter +1
+
+        if (export_accuracy) then
+            write(20, *) iter, diff
+        end if
+
+        if (iter == 100000) then
+            print*, "No root found after 100000 iterations! Giving out last values instead:" 
+            exit
+        end if
     enddo
 
     res%x_0 = midpoint
@@ -47,11 +61,16 @@ end function find_root_bisection
 
 
 
-function find_root_newton(func, func_derivative, init_guess) result(res)
+function find_root_newton(func, func_derivative, init_guess, export_accuracy) result(res)
     implicit none
     type(myroot_type) :: res
     real(kind=8) :: func, func_derivative, init_guess, func_value, func_value_derivative, epsilon_zero, x
     integer :: iter
+    logical :: export_accuracy
+
+    if (export_accuracy) then
+        open(20, file="data/accuracy_newton.dat")
+    end if
 
     epsilon_zero = 1
     x = init_guess
@@ -62,6 +81,15 @@ function find_root_newton(func, func_derivative, init_guess) result(res)
         epsilon_zero = -(func_value)/(func_value_derivative)
         x = x + epsilon_zero
         iter = iter+1
+
+        if (export_accuracy) then
+            write(20, *) iter, abs(epsilon_zero)
+        end if
+
+        if (iter == 100000) then
+            print*, "No root found after 100000 iterations! Giving out last values instead:" 
+            exit
+        end if
     enddo
 
     res%x_0 = x
