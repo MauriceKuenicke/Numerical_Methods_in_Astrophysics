0a64a328010c827b805f562c1f2ecba603be910f
Maurice Künicke <m.kuenicke1995@gmail.com>
2020-12-06 13:19:45 +0100

    session_04 skeleton

---
+++ session_04/Numerical_Integration/numericalIntegration_mod.f90
@@ -0,0 +1,20 @@
+module numericalIntegration_mod
+    implicit none
+    private
+
+
+
+
+    public :: trapezoidal
+
+
+contains
+
+function trapezoidal() result(res)
+    integer :: res
+
+end function trapezoidal
+
+
+
+end module numericalIntegration_mod
