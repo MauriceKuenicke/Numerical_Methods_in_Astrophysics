6a7cfd7f55e14d45804601fb525a00852edd707d
Maurice Künicke <m.kuenicke1995@gmail.com>
2020-12-09 00:10:21 +0100

    Header + Comments

--- session_04/Numerical_Integration/numericalIntegration_mod.f90
+++ session_04/Numerical_Integration/numericalIntegration_mod.f90
@@ -1,91 +1,114 @@
+! Module containing the trapezoidal and simpson's rule for numerical integrations. 
 module numericalIntegration_mod
     implicit none
     private
 
-    ! Double precision byte size
+    ! Declaration of module variables
     integer, parameter :: dp = kind(0.d0)
     real(dp)           :: trapezoid_area_old
     integer            :: recursion_level
 
-
+    ! Make functions public
     public :: trapez
     public :: simpson
 
 
 contains
 
+! Trapezoidal Rule for numerical integration
 function trapez(func, lower, upper, accuracy, export) result(trapezoid_area_new)
+    ! Declaration of local variables
     real(dp), external   :: func
     real(dp)             :: lower, upper, accuracy, h, relative_error, trapezoid_area_new, h_new
-    integer              :: evals
-    logical  :: export
+    integer              :: evals=0
+    logical              :: export
     
+    ! Initialize variables
     recursion_level = 0
     trapezoid_area_old = 0
-    h = upper - lower
-    trapezoid_area_old = 1._dp/2._dp * h * (func(lower) + func(upper))   ! Calculate inital estimate
 
+    ! Calculate initial h and area
+    h = upper - lower
+    trapezoid_area_old = 1._dp/2._dp * h * (func(lower) + func(upper))
+
+    ! Open file if export variable is set to true
     if (export) then
         open(20, file="data/accuracy_trapez.dat")
     end if
 
-    do  
-        h_new = (1._dp/2._dp)** recursion_level * h
-        trapezoid_area_new = trapez_next(func, h_new, lower, evals)
-        relative_error = abs((trapezoid_area_new - trapezoid_area_old)/trapezoid_area_new)
-        trapezoid_area_old = trapezoid_area_new
-        
 
+    do  
+        h_new = (1._dp/2._dp)** recursion_level * h                                           ! Reduce step size by a factor of two
+        trapezoid_area_new = trapez_next(func, h_new, lower, evals)                           ! Calculate next trapezoid area estimate
+        relative_error = abs((trapezoid_area_new - trapezoid_area_old)/trapezoid_area_new)    ! Calculate relative error
+        trapezoid_area_old = trapezoid_area_new                                               ! Set new estimate to be the old estimate for the next iteration
+        
+        ! Export
         if (export) then
             write(20, *) evals, relative_error
         end if
-        if (relative_error <= accuracy) exit
-        recursion_level = recursion_level + 1
-    end do
 
+        ! Exit loop if desired accuracy level is achieved or increase recursion_level by one
+        if (relative_error <= accuracy) then
+            exit
+        else
+            recursion_level = recursion_level + 1
+        end if
+    end do
 end function trapez
 
+! Function that calculates the next area estimate based on a given step size
 function trapez_next(func, h_new, lower, evals) result(trapezoid_area_new)
-    real(dp), external :: func
-    integer            :: i, nr_trapezoids
-    real(dp)           :: lower, h_new, trapezoid_area_new, area
-    integer, intent(out) :: evals
+    ! Declaration of local variables
+    real(dp), external      :: func
+    integer                 :: i, nr_trapezoids
+    real(dp)                :: lower, h_new, trapezoid_area_new, area
+    integer, intent(out)    :: evals                                      ! Give back the number of function evaluations for this level
     
-    nr_trapezoids = 2**(recursion_level)
-    area = 0
+    nr_trapezoids = 2**(recursion_level)                                  ! Calculate the number of trapezoids
+
+    ! Calculate the summation term
+    area = 0                                                              ! Reset area
     do i = 1, nr_trapezoids, 2
         area = area + func(lower+i*h_new)
         evals = evals + 1
     end do
-    trapezoid_area_new = (1._dp/2._dp)*trapezoid_area_old + h_new * area
 
+    ! Calculate the new trapezoid area using the old value
+    trapezoid_area_new = (1._dp/2._dp)*trapezoid_area_old + h_new * area
 end function trapez_next
 
+! Simpson's rule for numerical integration
 function simpson(func, lower, upper, accuracy, export) result(area)
+    ! Declaration of local variables
     real(dp), external      :: func
     real(dp)                :: area, lower, upper, accuracy, h, t_k, t_kplus, h_new, relative_error
-    integer :: evals
-    logical  :: export
-
-    recursion_level = 0
-    trapezoid_area_old = 0
-    h = upper - lower
-
+    integer                 :: evals=0
+    logical                 :: export
+    
+    
+    ! Open file if export variable is set to true
     if (export) then
         open(20, file="data/accuracy_simpson.dat")
     end if
     
-    trapezoid_area_old = 1._dp/2._dp * h * (func(lower) + func(upper))
-    h_new = (1._dp/2._dp)** recursion_level * h
-    t_k = trapez_next(func, h_new, lower, evals)
+    ! Initialize initial variables
+    recursion_level = 0                                                     ! Set initial recursion level
+    trapezoid_area_old = 0                                                  ! Set initial trapezoid area
+    h = upper - lower
+    trapezoid_area_old = 1._dp/2._dp * h * (func(lower) + func(upper))      ! Calculate initial  trapezoid area
+    h_new = (1._dp/2._dp)** recursion_level * h                             ! Calculate firest step size reduction
+    t_k = trapez_next(func, h_new, lower, evals)                            ! Calculate first extra step using the trapezoid rule
     trapezoid_area_old = t_k
+
+    ! use Simpson's rule to calculate the area estimate based on the trapezoid rule
     do  
         trapezoid_area_old = t_k
-
+        
+        ! Increase recursion level by one
         recursion_level = recursion_level + 1
         h_new = (1._dp/2._dp)** recursion_level * h
         
- 
         t_kplus = trapez_next(func, h_new, lower, evals)
         
         area = (4._dp/3._dp)*t_kplus - (1._dp/3._dp)*t_k
@@ -94,10 +117,11 @@ function simpson(func, lower, upper, accuracy, export) result(area)
         if (export) then
             write(20, *) evals, relative_error
         end if
-        if(relative_error < accuracy) exit
-        t_k = t_kplus
-    end do 
 
+        ! Exit loop if desired accuracy is achieved
+        if(relative_error < accuracy) exit
+        t_k = t_kplus                                              ! Set the newly calculated trapez area as the old area for the next iteration
+    end do 
 end function simpson
 
 end module numericalIntegration_mod
--- session_04/Numerical_Integration/test_integration.f90
+++ session_04/Numerical_Integration/test_integration.f90
@@ -1,9 +1,20 @@
 !!+ test_integration.f90
+!!  Calculate the integral of three test functions using the trapezoidal and Simpson's rule. Use a naive solution
+!!  for integrals with no upper bound. Export the results for Function 2 into the /data directory.
 !!
 !!  compile with: > gfortran -o test_integration numericalIntegration_mod.f90 myfuncs.f90 test_integration.f90
 !!  
 !!  usage: > ./test_integration
 !!
+!!  expected result:
+!!                  Function 1:          trapez:  6.000000447035
+!!                                       simpson: 6.000000298023
+!!
+!!                  Function 2:          trapez:  8.153364636096
+!!                                       simpson: 8.153364464001
+!!                     
+!!                  Function 3:          trapez:  0.499999955293
+!!                                       simpson: 0.499999985091
 !!-
 
 
@@ -13,20 +24,24 @@ program test_integration
     use myfuncs
     implicit none
 
+    ! Declaration of local variables
     integer, parameter :: dp = kind(0.d0)
     real(dp) :: res_1, res_2, open_solution_naive
     logical  :: export
     
+    ! Calculate results for test function 1
     export = .false.
-    res_1 = trapez(func1, 0._dp, 2._dp, 1e-7_dp, export)
-    res_2 = simpson(func1, 0._dp, 2._dp, 1e-7_dp, export)
+    res_1 = trapez(func1, 0._dp, 2._dp, 1e-7_dp, export)         ! trapez(function, lower_bound, upper_bound, accuracy, export)
+    res_2 = simpson(func1, 0._dp, 2._dp, 1e-7_dp, export)        ! simpson(function, lower_bound, upper_bound, accuracy, export)
     write(*, '("Function 1:", 10X, "trapez:  ", F14.12, / ,21X, "simpson: ", F14.12)') res_1, res_2
     
+    ! Calculate and export results for test function 2
     export = .true.
     res_1 = trapez(func2, 0._dp, 2._dp, 1e-7_dp, export)
     res_2 = simpson(func2, 0._dp, 2._dp, 1e-7_dp, export)
     write(*, '(/,"Function 2:", 10X, "trapez:  ", F14.12, / ,21X, "simpson: ", F14.12)') res_1, res_2
     
+    ! Calculate results for test function 3
     export = .false.
     res_1 = open_solution_naive(func3, 0._dp, 1e-7_dp, "trapez", export)
     res_2 = open_solution_naive(func3, 0._dp, 1e-7_dp, "simpson", export)
@@ -38,26 +53,32 @@ end program test_integration
 function open_solution_naive(func, lower, accuracy, algorithm, export) result(res)
     use numericalIntegration_mod
     implicit none
+
+    ! Declaration of local virables
     integer, parameter :: dp = kind(0.d0)
     real(dp), external :: func
     character(len=*)   :: algorithm
-    real(dp)           ::  j, res_new, error, lower, accuracy, res
-    logical  :: export
+    real(dp)           :: upper_bound, res_new, error, lower, accuracy, res
+    logical            :: export
 
-    j = 1._dp
+    ! Initialize values
     res = 1._dp
     res_new = 0._dp
+
+    ! Increase upper bound j until changes in area are smaller than the given accuracy
+    upper_bound = 1._dp
     do  
         if (algorithm == "trapez") then
-            res_new = trapez(func, lower, j, accuracy, export)
+            res_new = trapez(func, lower, upper_bound, accuracy, export)
         end if
         if (algorithm == "simpson") then
-            res_new = simpson(func, lower, j, accuracy, export)
+            res_new = simpson(func, lower, upper_bound, accuracy, export)
         end if
-        j = j + 1._dp
-        error = abs(res_new - res)
+        upper_bound = upper_bound + 1._dp
+
+        error = abs((res_new - res)/res_new)
         res = res_new
-        if (error <  1e-6_dp) exit
+        if (error <  1e-6_dp) exit               ! exit loop if desired accuracy is achieved
     end do
 
 end function open_solution_naive
